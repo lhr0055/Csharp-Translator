02cfc50d570750594e949c767a9392942dbcf47b
lhr0055 <lhr0055@naver.com>
2024-05-27 16:49:20 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace mome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnT = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "Kor",
+            "Eng",
+            "Jap",
+            "Chi"});
+            this.cb1.Location = new System.Drawing.Point(311, 126);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(273, 20);
+            this.cb1.TabIndex = 4;
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.cb1_SelectedIndexChanged);
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txt1.Location = new System.Drawing.Point(21, 152);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt1.Size = new System.Drawing.Size(273, 351);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(311, 152);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt2.Size = new System.Drawing.Size(273, 351);
+            this.txt2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.ForeColor = System.Drawing.Color.OliveDrab;
+            this.button3.Location = new System.Drawing.Point(311, 509);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 32);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "URL";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnT
+            // 
+            this.btnT.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.btnT.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnT.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnT.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnT.Location = new System.Drawing.Point(489, 86);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(95, 34);
+            this.btnT.TabIndex = 10;
+            this.btnT.Text = "translate";
+            this.btnT.UseVisualStyleBackColor = false;
+            this.btnT.Click += new System.EventHandler(this.btnT_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Hobo BT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label1.Location = new System.Drawing.Point(101, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 30);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Olive Translator";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(22, 510);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(134, 32);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(39, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 51);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::mome.Properties.Resources.스크린샷_2024_05_23_2044131;
+            this.pictureBox1.Location = new System.Drawing.Point(213, 509);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 32);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(603, 152);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt3.Size = new System.Drawing.Size(273, 351);
+            this.txt3.TabIndex = 15;
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Items.AddRange(new object[] {
+            "Kor",
+            "Eng",
+            "Jap",
+            "Chi"});
+            this.cb2.Location = new System.Drawing.Point(603, 126);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(273, 20);
+            this.cb2.TabIndex = 14;
+            this.cb2.SelectedIndexChanged += new System.EventHandler(this.cb2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(899, 552);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.cb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.ComboBox cb2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using IronOcr;
+using Google.Apis.Services;
+using Google.Apis.Translate.v2;
+using Google.Apis.Translate.v2.Data;
+using System.Linq.Expressions;
+
+namespace mome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Initializecb(cb1);
+            Initializecb(cb2);
+        }
+        private void Initializecb(ComboBox cb)
+        {
+            cb.Items.Clear();
+            cb.Items.Add("ko");
+            cb.Items.Add("en");
+            cb.Items.Add("zh");
+            cb.Items.Add("ja");
+            cb.Items.Add("es");
+            cb.SelectedIndex = 0;
+        }
+
+        private void openImgToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //이미지 파일 가져오기
+                pictureBox3.Image = new Bitmap(ofd.FileName);
+
+                //이미지 텍스트 추출
+                Bitmap oc = (Bitmap)pictureBox3.Image.Clone();
+                string ocr = OCRprocess(oc);
+
+                //텍스트박스 입력
+                txt1.Clear();
+                txt1.AppendText(ocr.Replace("\n", "\r\n") + "\r\n");
+              
+            }
+        }
+        public string OCRprocess(Bitmap oc)
+        {
+            try
+            {   //tessdata를 저장했던 경로를 지정
+                //string tessdataPath = Application.StartupPath + @"\tessdata";
+                //string tessdataPath = @"C:\Program Files\Tesseract-OCR\tessdata";
+                // 환경 변수로 tessdata 경로 설정
+                Environment.SetEnvironmentVariable("TESSDATA_PREFIX", @"C:\Program Files\Tesseract-OCR\");
+
+                string ocrLang = "eng+jpn+chi_sim+kor";
+
+                //Engine을 통해 OCR 실행 
+                //using (var engine = new Tesseract.TesseractEngine(tessdataPath, "eng", Tesseract.EngineMode.TesseractOnly))
+                using (var engine = new Tesseract.TesseractEngine(@"C:\Program Files\Tesseract-OCR\tessdata", ocrLang, Tesseract.EngineMode.Default))
+                {
+                    using(var page = engine.Process(oc))
+                    {
+                        return page.GetText();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return "OCR Error: " + ex.Message + "\nStack Trace: " + ex.StackTrace;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnT_Click(object sender, EventArgs e)
+        {
+            TranslateService ts = new TranslateService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyAd4kYy0DoY9E2piz77LcXCXau6-TGPmjA",
+                ApplicationName = ""
+            });
+            string originalString = txt1.Text;//txt1의 텍스트
+            string lang = cb1.SelectedItem.ToString(); //번역할 언어 
+            string lang2 = cb2.SelectedItem.ToString(); //번역할 언어 
+
+            try
+            {
+                //번역1 요청
+                TranslationsListResponse response = ts.Translations.List(originalString, lang).Execute();
+                //번역1 결과
+                txt2.Clear();
+                txt2.Text = response.Translations[0].TranslatedText;
+
+                //번역2 요청
+                TranslationsListResponse response2 = ts.Translations.List(originalString, lang2).Execute();
+                //번역2 결과
+                txt3.Clear();
+                txt3.Text = response2.Translations[0].TranslatedText;
+            }
+            catch (Exception ex)
+            {
+                txt2.Text = "Translator Error" + ex.Message;
+            }
+        }
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cb2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
