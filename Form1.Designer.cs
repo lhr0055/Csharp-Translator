11747e86b7da7b1437c18ff4c548672bff9ff804
lhr0055 <lhr0055@naver.com>
2024-05-29 13:40:19 +0900

    code1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.cb1 = new System.Windows.Forms.ComboBox();
             this.txt1 = new System.Windows.Forms.TextBox();
             this.txt2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.btnT = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.fontDialog1 = new System.Windows.Forms.FontDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txt3 = new System.Windows.Forms.TextBox();
             this.cb2 = new System.Windows.Forms.ComboBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ckhide = new System.Windows.Forms.CheckBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // cb1
@@ -58,7 +63,7 @@
             "Eng",
             "Jap",
             "Chi"});
-            this.cb1.Location = new System.Drawing.Point(311, 126);
+            this.cb1.Location = new System.Drawing.Point(388, 126);
             this.cb1.Name = "cb1";
             this.cb1.Size = new System.Drawing.Size(273, 20);
             this.cb1.TabIndex = 4;
@@ -67,7 +72,8 @@
             // txt1
             // 
             this.txt1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.txt1.Location = new System.Drawing.Point(21, 152);
+            this.txt1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt1.Location = new System.Drawing.Point(99, 152);
             this.txt1.Multiline = true;
             this.txt1.Name = "txt1";
             this.txt1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -76,36 +82,31 @@
             // 
             // txt2
             // 
-            this.txt2.Location = new System.Drawing.Point(311, 152);
+            this.txt2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt2.Location = new System.Drawing.Point(388, 152);
             this.txt2.Multiline = true;
             this.txt2.Name = "txt2";
             this.txt2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txt2.Size = new System.Drawing.Size(273, 351);
             this.txt2.TabIndex = 7;
             // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.ForeColor = System.Drawing.Color.OliveDrab;
-            this.button3.Location = new System.Drawing.Point(311, 509);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(82, 32);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "URL";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
             // btnT
             // 
             this.btnT.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
             this.btnT.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.btnT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnT.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnT.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnT.Location = new System.Drawing.Point(489, 86);
+            this.btnT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnT.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnT.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnT.FlatAppearance.BorderSize = 0;
+            this.btnT.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnT.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnT.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnT.Location = new System.Drawing.Point(575, 94);
+            this.btnT.Margin = new System.Windows.Forms.Padding(0);
             this.btnT.Name = "btnT";
-            this.btnT.Size = new System.Drawing.Size(95, 34);
+            this.btnT.Size = new System.Drawing.Size(86, 29);
             this.btnT.TabIndex = 10;
-            this.btnT.Text = "translate";
+            this.btnT.Text = "Translate";
             this.btnT.UseVisualStyleBackColor = false;
             this.btnT.Click += new System.EventHandler(this.btnT_Click);
             // 
@@ -114,7 +115,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Hobo BT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.OliveDrab;
-            this.label1.Location = new System.Drawing.Point(101, 27);
+            this.label1.Location = new System.Drawing.Point(376, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(197, 30);
             this.label1.TabIndex = 12;
@@ -138,7 +139,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Black;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(39, 13);
+            this.pictureBox2.Location = new System.Drawing.Point(315, 6);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(63, 51);
             this.pictureBox2.TabIndex = 11;
@@ -147,20 +148,19 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::mome.Properties.Resources.스크린샷_2024_05_23_2044131;
-            this.pictureBox1.Location = new System.Drawing.Point(213, 509);
+            this.pictureBox1.Location = new System.Drawing.Point(44, 62);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(10, 17);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // txt3
             // 
-            this.txt3.Location = new System.Drawing.Point(603, 152);
+            this.txt3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt3.Location = new System.Drawing.Point(680, 152);
             this.txt3.Multiline = true;
             this.txt3.Name = "txt3";
             this.txt3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -175,35 +175,107 @@
             "Eng",
             "Jap",
             "Chi"});
-            this.cb2.Location = new System.Drawing.Point(603, 126);
+            this.cb2.Location = new System.Drawing.Point(680, 126);
             this.cb2.Name = "cb2";
             this.cb2.Size = new System.Drawing.Size(273, 20);
             this.cb2.TabIndex = 14;
             this.cb2.SelectedIndexChanged += new System.EventHandler(this.cb2_SelectedIndexChanged);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Controls.Add(this.ckhide);
+            this.panel.Controls.Add(this.button1);
+            this.panel.Controls.Add(this.pictureBox1);
+            this.panel.Controls.Add(this.button2);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(170, 551);
+            this.panel.TabIndex = 18;
+            this.panel.MouseHover += new System.EventHandler(this.panel1_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 56);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "PDF";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(0, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 56);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ckhide
+            // 
+            this.ckhide.Appearance = System.Windows.Forms.Appearance.Button;
+            this.ckhide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ckhide.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ckhide.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.ckhide.FlatAppearance.BorderSize = 0;
+            this.ckhide.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.ckhide.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.ckhide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ckhide.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ckhide.Location = new System.Drawing.Point(0, 525);
+            this.ckhide.Name = "ckhide";
+            this.ckhide.Size = new System.Drawing.Size(168, 24);
+            this.ckhide.TabIndex = 22;
+            this.ckhide.Text = "<";
+            this.ckhide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ckhide.UseVisualStyleBackColor = true;
+            this.ckhide.CheckedChanged += new System.EventHandler(this.ckhide_CheckedChanged);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(899, 552);
+            this.ClientSize = new System.Drawing.Size(988, 551);
+            this.Controls.Add(this.panel);
             this.Controls.Add(this.txt3);
+            this.Controls.Add(this.btnT);
             this.Controls.Add(this.cb2);
+            this.Controls.Add(this.txt1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.btnT);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txt2);
-            this.Controls.Add(this.txt1);
             this.Controls.Add(this.cb1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Translator";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,7 +286,6 @@
         private System.Windows.Forms.ComboBox cb1;
         private System.Windows.Forms.TextBox txt1;
         private System.Windows.Forms.TextBox txt2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnT;
         private System.Windows.Forms.Label label1;
@@ -223,10 +294,14 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.ColorDialog colorDialog2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.TextBox txt3;
         private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ckhide;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,16 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tesseract;
-using IronOcr;
+using PdfiumViewer;
 using Google.Apis.Services;
 using Google.Apis.Translate.v2;
 using Google.Apis.Translate.v2.Data;
 using System.Linq.Expressions;
+using System.Net.Sockets;
 
 namespace mome
 {
     public partial class Form1 : Form
     {
+        const int MAX_WIDTH = 170;
+        const int MIN_WIDTH = 50;
+        //슬라이딩 메뉴 속도
+        const int SLIDING = 10;
+        //최초 슬라이딩 메뉴 크기
+        int slide = 170;
         public Form1()
         {
             InitializeComponent();
@@ -27,25 +34,14 @@ namespace mome
         private void Initializecb(ComboBox cb)
         {
             cb.Items.Clear();
-            cb.Items.Add("ko");
-            cb.Items.Add("en");
-            cb.Items.Add("zh");
-            cb.Items.Add("ja");
-            cb.Items.Add("es");
+            cb.Items.AddRange(new string[] { "ko", "en", "zh", "ja", "es" });
             cb.SelectedIndex = 0;
         }
 
-        private void openImgToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void openImgToolStripMenuItem_Click(object sender, EventArgs e) { }
+        private void pictureBox2_Click(object sender, EventArgs e) { }
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == DialogResult.OK)
@@ -60,7 +56,8 @@ namespace mome
                 //텍스트박스 입력
                 txt1.Clear();
                 txt1.AppendText(ocr.Replace("\n", "\r\n") + "\r\n");
-              
+
+                ckhide.Checked = true;
             }
         }
         public string OCRprocess(Bitmap oc)
@@ -68,10 +65,11 @@ namespace mome
             try
             {   //tessdata를 저장했던 경로를 지정
                 //string tessdataPath = Application.StartupPath + @"\tessdata";
-                //string tessdataPath = @"C:\Program Files\Tesseract-OCR\tessdata";
+                //string tessdataPath = @"C:\Program Files\Tesseract-OCR\tessdata"; //환경변수코드를 입력해주기 
                 // 환경 변수로 tessdata 경로 설정
                 Environment.SetEnvironmentVariable("TESSDATA_PREFIX", @"C:\Program Files\Tesseract-OCR\");
 
+                //여러 언어 인식 
                 string ocrLang = "eng+jpn+chi_sim+kor";
 
                 //Engine을 통해 OCR 실행 
@@ -90,10 +88,7 @@ namespace mome
             }
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-
-        }
+        private void pictureBox3_Click(object sender, EventArgs e) { }
 
         private void btnT_Click(object sender, EventArgs e)
         {
@@ -126,29 +121,102 @@ namespace mome
             }
         }
 
-        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void cb2_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void panel1_MouseHover(object sender, EventArgs e) { }
+        private void ckhide_CheckedChanged(object sender, EventArgs e)
         {
-
+            if(ckhide.Checked) 
+            {
+                //슬라이딩 메뉴가 접혔을 때, 메뉴 버튼의 표시 
+                //Image btnimg2 = Image.FromFile(@"C:\Users\KOSTA\Desktop\Project Folder\C#\img\pdf.png");
+                button2.Text = "pdf";
+                //Image btnimg1 = Image.FromFile(@"C:\Users\KOSTA\Desktop\Project Folder\C#\img\image.png");
+                button1.Text = "img";
+                ckhide.Text = ">";
+            }
+            else
+            {
+                //슬라이딩 메뉴가 보였을 때, 메뉴 버튼의 표시
+                button2.Text = "PDF";
+                button2.Image = null;
+                button1.Text = "Image";
+                button1.Image = null;
+                ckhide.Text = "<";
+            }
+            timer.Start();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void timer_Tick(object sender, EventArgs e)
         {
-
+            if(ckhide.Checked)
+            {
+                //슬라이딩 메뉴 숨기는 동작
+                slide -= SLIDING;
+                if (slide <= MIN_WIDTH)
+                    timer.Stop();
+            }
+            else
+            {
+                //슬라이딩 메뉴 보이는 동작 
+                slide += SLIDING;
+                if(slide >= MAX_WIDTH)
+                    timer.Stop();
+            }
+            panel.Width = slide;
         }
 
-        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
 
+        //pdf 파일 업로드 버튼 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialg = new OpenFileDialog();
+            openFileDialg.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            if(openFileDialg.ShowDialog() == DialogResult.OK)
+            {
+                string pdfPath = openFileDialg.FileName;
+                string ocrText = ExtractTextFromPdf(pdfPath);
+                txt1.Text = ocrText;
+
+                ckhide.Checked = true;
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        //pdf 불러오기 
+        private string ExtractTextFromPdf(string pdfPath)
         {
-
-        }
-
-        private void cb2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            try
+            {
+                using(var document = PdfiumViewer.PdfDocument.Load(pdfPath))
+                {
+                    txt1.Clear();
+                    string resultText = "";
+                    for (int pageIndex= 0; pageIndex < document.PageCount; pageIndex++)
+                    {
+                        using(var page=document.Render(pageIndex, 300,300,true))
+                        {
+                            using (var bitmap = new Bitmap(page))
+                            {
+                                string ocrText = OCRprocess(bitmap);
+                                string[] lines = ocrText.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                                foreach(var line in lines)
+                                {
+                                    txt1.AppendText(line+Environment.NewLine);
+                                }
+                                resultText += ocrText + "\n";
+                            }
+                        }
+                    }
+                    return resultText;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message + "\nStack Trace: " + ex.StackTrace;
+            }
         }
     }
 }
